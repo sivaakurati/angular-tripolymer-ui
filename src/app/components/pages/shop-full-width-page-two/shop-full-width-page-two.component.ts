73cfed41a67a28e81cba2ea821862d6484163ba6
Raghu Chowdareddy <ragavareddy82@gmail.com>
2021-07-07 10:58:12 +0530

    added images

--- src/app/components/pages/shop-full-width-page-two/shop-full-width-page-two.component.ts
+++ src/app/components/pages/shop-full-width-page-two/shop-full-width-page-two.component.ts
@@ -30,13 +30,13 @@ export class ShopFullWidthPageTwoComponent implements OnInit {
             detailsLink: 'Urine collection bottle'
         },
         {
-            mainImg: 'assets/img/products/jar-bottle-cap.png',
+            mainImg: 'assets/img/products/jar-cap-white-1.png',
             title: '20 Liter Jar bottle caps ',
             newPrice: '0.60',
             oldPrice: '',
             sale: 'Sale!',
             outOfStock: '',
-            detailsLink: '20 Liter Jar bottle caps'
+            detailsLink: '20 Liter Jar bottle caps White'
         },
         {
             mainImg: 'assets/img/products/thread-cap-4.png',
@@ -46,6 +46,61 @@ export class ShopFullWidthPageTwoComponent implements OnInit {
             sale: '',
             outOfStock: '',
             detailsLink: 'Thread caps for big and small size'
+         }
+         ,
+        {
+            mainImg: 'assets/img/products/jar-cap-purple.png',
+            title: '20 Liter Jar bottle caps Purple',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '20 Liter Jar bottle caps Purple'
+         },
+        {
+            mainImg: 'assets/img/products/jar-cap-blue-1.png',
+            title: '20 Liter Jar bottle caps Blue',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '20 Liter Jar bottle caps Blue'
+         },
+        {
+            mainImg: 'assets/img/products/jar-cap-light-green-1.png',
+            title: '20 Liter Jar bottle caps Light Green',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '20 Liter Jar bottle caps Light Green'
+         },
+        {
+            mainImg: 'assets/img/products/jar-cap-green-1.png',
+            title: '20 Liter Jar bottle caps Green',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '20 Liter Jar bottle caps Green'
+         },
+        {
+            mainImg: 'assets/img/products/1-liter-customised-bottle-cap-1.png',
+            title: '1 Liter bottle caps ',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '1 Liter bottle caps '
+         },
+        {
+            mainImg: 'assets/img/products/half-liter-customized-cap-1.png',
+            title: '1/2 Liter bottle caps ',
+            newPrice: '2.00',
+            oldPrice: '',
+            sale: '',
+            outOfStock: '',
+            detailsLink: '1/2 Liter bottle caps '
          }//,
         // {
         //     mainImg: 'assets/img/products/products7.jpg',
